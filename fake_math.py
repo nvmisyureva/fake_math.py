2aac5bff1445cd6844bf7d00c136a492f38c9ae8
nvmisyureva <168543591+nvmisyureva@users.noreply.github.com>
2024-10-13 10:59:02 +0500

    Add files via upload

---
+++ fake_math.py
@@ -0,0 +1,16 @@
+
+
+def divide1_x_y():
+
+    x = int(input('Введите число x:'))
+    y = int(input('Введите число y:'))
+
+    if y == 0:
+        print("Ошибка" )
+
+    else:
+        print (x/y)
+
+
+result1 = divide1_x_y()
+
